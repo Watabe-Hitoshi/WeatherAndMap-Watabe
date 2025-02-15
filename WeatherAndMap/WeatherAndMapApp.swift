c66ff62f73f0c22fd444e0d6c355ffa6c0e80f1a
Hitoshi <hitoshiwtb1104@gmail.com>
2025-02-15 16:27:47 +0900

    Initial Commit

---
+++ WeatherAndMap/WeatherAndMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAndMapApp.swift
+//  WeatherAndMap
+//
+//  Created by hitoshi on 2025/02/15.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAndMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
